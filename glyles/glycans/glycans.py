d4abaf6d739908fa390a09b7cdb4fc7b0ed8b45c
Roman <joeres.roman@web.de>
2021-10-20 13:21:29 +0200

    SMILES for valid glycans (removed Glu)

--- glyles/glycans/glycans.py
+++ glyles/glycans/glycans.py
@@ -2,11 +2,14 @@ from enum import Enum
 
 
 class Glycan(Enum):
-    GLC = ("Glc")
-    GLU = ("Glu")
-    FRU = ("Fru")
-    MAN = ("Man")
-    GAL = ("Gal")
+    GLC = ("Glc", "C([C@@H]1[C@@H]([C@@H]([C@H](C(O1)O)O)O)O)O")
+    FRU = ("Fru", "C1[C@H]([C@H]([C@@H](C(O1)(CO)O)O)O)O")
+    MAN = ("Man", "OC[C@H]1O[C@H](O)[C@@H](O)[C@@H](O)[C@@H]1O")
+    GAL = ("Gal", "OC[C@H]1O[C@H](O)[C@H](O)[C@@H](O)[C@H]1O")
+
+    def __init__(self, name, smiles):
+        self.name = name
+        self.smiles = smiles
 
 
 def from_string(mono: str):
