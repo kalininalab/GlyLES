73d34c8eefae431c40890371cdb6f0704a9444b2
Roman <joeres.roman@web.de>
2021-10-21 13:02:54 +0200

    Bug fixes according to tests

--- glyles/glycans/glycans.py
+++ glyles/glycans/glycans.py
@@ -2,16 +2,20 @@ from enum import Enum
 
 
 class Glycan(Enum):
-    GLC = ("Glc", "C([C@@H]1[C@@H]([C@@H]([C@H](C(O1)O)O)O)O)O")
-    FRU = ("Fru", "C1[C@H]([C@H]([C@@H](C(O1)(CO)O)O)O)O")
-    MAN = ("Man", "OC[C@H]1O[C@H](O)[C@@H](O)[C@@H](O)[C@@H]1O")
-    GAL = ("Gal", "OC[C@H]1O[C@H](O)[C@H](O)[C@@H](O)[C@H]1O")
-    XYL = ("Xyl", "C1[C@H]([C@@H]([C@H](C(O1)O)O)O)O")
-
-    def __init__(self, name, smiles):
-        self.name = name
-        self.smiles = smiles
+    GLC = {"name": "Glc", "smiles": "C([C@@H]1[C@@H]([C@@H]([C@H](C(O1)O)O)O)O)O"}
+    FRU = {"name": "Fru", "smiles": "C1[C@H]([C@H]([C@@H](C(O1)(CO)O)O)O)O"}
+    MAN = {"name": "Man", "smiles": "OC[C@H]1O[C@H](O)[C@@H](O)[C@@H](O)[C@@H]1O"}
+    GAL = {"name": "Gal", "smiles": "OC[C@H]1O[C@H](O)[C@H](O)[C@@H](O)[C@H]1O"}
+    XYL = {"name": "Xyl", "smiles": "C1[C@H]([C@@H]([C@H](C(O1)O)O)O)O"}
 
 
-def from_string(mono: str):
+def from_string(mono):
+    """
+
+    Args:
+        mono (str):
+
+    Returns:
+        Glycan according to the monosaccharide provided via mono
+    """
     return Glycan[mono.upper()]
