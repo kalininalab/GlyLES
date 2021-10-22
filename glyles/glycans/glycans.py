f1e40b173c7411d88e962866d51c7b5546bcb0fb
Roman <joeres.roman@web.de>
2021-10-22 15:41:41 +0200

    Smiles writer completed (but NOT tested)

--- glyles/glycans/glycans.py
+++ glyles/glycans/glycans.py
@@ -4,9 +4,10 @@ import networkx as nx
 
 
 class Chirality(Enum):
-    CC = "@"  # in Haworth notation up
-    C = "@@"  # in Haworth notation down
-    N = "No"
+    # relative to haworth notation
+    UP = 1
+    DOWN = 2
+    NONE = 3
 
 
 class Atom(Enum):
@@ -29,54 +30,55 @@ class Glycan(Enum):
         # TODO: Implement a parser for SMILES -> smiles.smiles.SMILES.read()
         g = nx.Graph()
         g.add_nodes_from([
-            (1, {"type": Atom.C, "chiral": Chirality.N}),
-            (2, {"type": Atom.C, "chiral": Chirality.N}),
-            (3, {"type": Atom.C, "chiral": Chirality.N}),
-            (4, {"type": Atom.C, "chiral": Chirality.N}),
-            (5, {"type": Atom.C, "chiral": Chirality.N}),
-            (6, {"type": Atom.C, "chiral": Chirality.N}),
-            (10, {"type": Atom.O, "chiral": Chirality.N}),
-            (11, {"type": Atom.O, "chiral": Chirality.N}),
-            (12, {"type": Atom.O, "chiral": Chirality.N}),
-            (13, {"type": Atom.O, "chiral": Chirality.N}),
-            (14, {"type": Atom.O, "chiral": Chirality.N}),
-            (15, {"type": Atom.O, "chiral": Chirality.N}),
+            (1, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+            (2, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+            (3, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+            (4, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+            (5, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+            (6, {"type": Atom.C, "chiral": Chirality.NONE, "ring": False}),
+            (10, {"type": Atom.O, "chiral": Chirality.NONE, "ring": True}),
+            (11, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+            (12, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+            (13, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+            (14, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+            (15, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
         ])
         if self == Glycan.GLC:
             g.add_edges_from([
                 (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
                 (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
             ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.C}, 3: {"chiral": Chirality.CC},
-                                       4: {"chiral": Chirality.C}, 5: {"chiral": Chirality.CC}})
+            nx.set_node_attributes(g, {1: {"ring": False},
+                                       2: {"chiral": Chirality.DOWN}, 3: {"chiral": Chirality.UP},
+                                       4: {"chiral": Chirality.DOWN}, 5: {"chiral": Chirality.UP}})
         elif self == Glycan.FRU:
             g.add_edges_from([
                 (10, 2), (2, 3), (3, 4), (4, 5), (5, 10),
                 (1, 2), (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
             ])
-            nx.set_node_attributes(g, {3: {"chiral": Chirality.CC}, 4: {"chiral": Chirality.C},
-                                       5: {"chiral": Chirality.CC}})
+            nx.set_node_attributes(g, {3: {"chiral": Chirality.UP}, 4: {"chiral": Chirality.DOWN},
+                                       5: {"chiral": Chirality.UP}})
         elif self == Glycan.MAN:
             g.add_edges_from([
                 (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
                 (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15)
             ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.CC}, 3: {"chiral": Chirality.CC},
-                                       4: {"chiral": Chirality.C}, 5: {"chiral": Chirality.CC}})
+            nx.set_node_attributes(g, {2: {"chiral": Chirality.UP}, 3: {"chiral": Chirality.UP},
+                                       4: {"chiral": Chirality.DOWN}, 5: {"chiral": Chirality.UP}})
         elif self == Glycan.GAL:
             g.add_edges_from([
                 (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
                 (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
             ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.C}, 3: {"chiral": Chirality.CC},
-                                       4: {"chiral": Chirality.CC}, 5: {"chiral": Chirality.CC}})
+            nx.set_node_attributes(g, {2: {"chiral": Chirality.DOWN}, 3: {"chiral": Chirality.UP},
+                                       4: {"chiral": Chirality.UP}, 5: {"chiral": Chirality.UP}})
         elif self == Glycan.TAL:
             g.add_edges_from([
                 (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
                 (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
             ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.CC}, 3: {"chiral": Chirality.CC},
-                                       4: {"chiral": Chirality.CC}, 5: {"chiral": Chirality.CC}})
+            nx.set_node_attributes(g, {2: {"chiral": Chirality.UP}, 3: {"chiral": Chirality.UP},
+                                       4: {"chiral": Chirality.UP}, 5: {"chiral": Chirality.UP}})
         return g
 
 
--- glyles/smiles/smiles.py
+++ glyles/smiles/smiles.py
@@ -3,33 +3,49 @@ import networkx as nx
 from glyles.glycans.glycans import Glycan, Chirality
 
 
+class DFS:
+    def dfs_tree(self, g, source):
+        dfst = nx.DiGraph()
+        self.__dfs(g, dfst, None, source)
+
+        return dfst
+
+    def __dfs(self, g, dfst, parent, node):
+        dfst.add_node(node)
+
+        if parent is not None:
+            dfst.add_edge(parent, node)
+
+        children = sorted([x[1] for x in list(g.edges(node))], key=lambda x: (not g.nodes[x]["ring"], x))
+        for child in children:
+            if child not in dfst.nodes:
+                self.__dfs(g, dfst, node, child)
+
+
 class SMILES:
     def __init__(self):
         pass
 
     def write(self, g, source):
-        # TODO: Reimplement DFS to get a search along the inner ring in clockwise manner
-        dfs_tree = nx.algorithms.dfs_tree(g, source)
-        path = nx.algorithms.dag.dag_longest_path(dfs_tree)
-        return self.__construct(g, dfs_tree, path, source, first=True)
+        dfs_tree = DFS().dfs_tree(g, source)
+        return self.__construct(g, dfs_tree, source, first=True)
 
     def read(self):
         pass
 
-    def __construct(self, g, tree, path, node, first=False):
+    def __construct(self, g, tree, node, first=False):
         """
 
         Args:
             g (nx.Graph): graph of the molecule
             tree (nx.DiGraph): dfs-tree on that molecule
-            path (list): longest path in tree
             node (int): node id the construction is currently in
 
         Returns:
             SMILES representation of the subtree of the current node in tree
         """
-        if g.nodes[node]["chiral"] != Chirality.N:  # chirality
-            output = "[C{value}H]".format(value=g.nodes[node]["chiral"].value)
+        if g.nodes[node]["chiral"] != Chirality.NONE:  # chirality
+            output = "[C{value}H]".format(value=("@@" if g.nodes[node]["chiral"] == Chirality.DOWN else "@"))
         else:
             output = g.nodes[node]["type"].value
 
@@ -40,13 +56,9 @@ class SMILES:
         if len(children) == 0:  # leaf
             return output
         else:  # children
-            path_one = -1
             for c in [x[1] for x in children]:
-                if c in path:
-                    path_one = c
-                    continue
-                output += "({smiles})".format(smiles=self.__construct(g, tree, path, c))
-            return output + ("" if path_one == -1 else self.__construct(g, tree, path, path_one))
+                output += "({smiles})".format(smiles=self.__construct(g, tree, c))
+            return output
 
 
 print(SMILES().write(Glycan.GLC.structure(), 1))
