5160b9e302612e0d29c9e47e6b0148d9e90d7f94
Roman <joeres.roman@web.de>
2021-10-25 14:53:12 +0200

    SMILES merger implemented and bug spotted

--- glyles/glycans/glycans.py
+++ glyles/glycans/glycans.py
@@ -20,66 +20,69 @@ class Atom(Enum):
 
 
 class Glycan(Enum):
-    GLC = {"name": "Glc", "smiles": "C([C@@H]1[C@H]([C@@H]([C@H](C(O1)O)O)O)O)O"}  # from c5 to c1
-    FRU = {"name": "Fru", "smiles": "C([C@@H]1[C@H]([C@@H](C(O1)(CO)O)O)O)O"}
-    MAN = {"name": "Man", "smiles": "C([C@@H]1[C@H]([C@@H]([C@@H](C(O1)O)O)O)O)O"}
-    GAL = {"name": "Gal", "smiles": "C([C@@H]1[C@@H]([C@@H]([C@H](C(O1)O)O)O)O)O"}
-    TAL = {"name": "Tal", "smiles": "C([C@@H]1[C@@H]([C@@H]([C@@H](C(O1)O)O)O)O)O"}
+    GLC = {"name": "Glc", "smiles": "C([C@@H]1[C@H]([C@@H]([C@H](C(O1)O)O)O)O)O", "struct": None}
+    FRU = {"name": "Fru", "smiles": "C([C@@H]1[C@H]([C@@H](C(O1)(CO)O)O)O)O", "struct": None}
+    MAN = {"name": "Man", "smiles": "C([C@@H]1[C@H]([C@@H]([C@@H](C(O1)O)O)O)O)O", "struct": None}
+    GAL = {"name": "Gal", "smiles": "C([C@@H]1[C@@H]([C@@H]([C@H](C(O1)O)O)O)O)O", "struct": None}
+    TAL = {"name": "Tal", "smiles": "C([C@@H]1[C@@H]([C@@H]([C@@H](C(O1)O)O)O)O)O", "struct": None}
 
     def structure(self):
-        # TODO: Implement a parser for SMILES -> smiles.smiles.SMILES.read()
-        g = nx.Graph()
-        g.add_nodes_from([
-            (1, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
-            (2, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
-            (3, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
-            (4, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
-            (5, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
-            (6, {"type": Atom.C, "chiral": Chirality.NONE, "ring": False}),
-            (10, {"type": Atom.O, "chiral": Chirality.NONE, "ring": True}),
-            (11, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
-            (12, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
-            (13, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
-            (14, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
-            (15, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
-        ])
-        if self == Glycan.GLC:
-            g.add_edges_from([
-                (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
-                (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
+        if self.value["struct"] is None:
+            # TODO: Implement a parser for SMILES -> smiles.smiles.SMILES.read()
+            g = nx.Graph()
+            g.add_nodes_from([
+                (1, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+                (2, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+                (3, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+                (4, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+                (5, {"type": Atom.C, "chiral": Chirality.NONE, "ring": True}),
+                (6, {"type": Atom.C, "chiral": Chirality.NONE, "ring": False}),
+                (10, {"type": Atom.O, "chiral": Chirality.NONE, "ring": True}),
+                (11, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+                (12, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+                (13, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+                (14, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
+                (15, {"type": Atom.O, "chiral": Chirality.NONE, "ring": False}),
             ])
-            nx.set_node_attributes(g, {1: {"ring": False},
-                                       2: {"chiral": Chirality.DOWN}, 3: {"chiral": Chirality.UP},
-                                       4: {"chiral": Chirality.DOWN}, 5: {"chiral": Chirality.UP}})
-        elif self == Glycan.FRU:
-            g.add_edges_from([
-                (10, 2), (2, 3), (3, 4), (4, 5), (5, 10),
-                (1, 2), (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
-            ])
-            nx.set_node_attributes(g, {3: {"chiral": Chirality.UP}, 4: {"chiral": Chirality.DOWN},
-                                       5: {"chiral": Chirality.UP}})
-        elif self == Glycan.MAN:
-            g.add_edges_from([
-                (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
-                (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15)
-            ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.UP}, 3: {"chiral": Chirality.UP},
-                                       4: {"chiral": Chirality.DOWN}, 5: {"chiral": Chirality.UP}})
-        elif self == Glycan.GAL:
-            g.add_edges_from([
-                (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
-                (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
-            ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.DOWN}, 3: {"chiral": Chirality.UP},
-                                       4: {"chiral": Chirality.UP}, 5: {"chiral": Chirality.UP}})
-        elif self == Glycan.TAL:
-            g.add_edges_from([
-                (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
-                (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
-            ])
-            nx.set_node_attributes(g, {2: {"chiral": Chirality.UP}, 3: {"chiral": Chirality.UP},
-                                       4: {"chiral": Chirality.UP}, 5: {"chiral": Chirality.UP}})
-        return g
+            if self == Glycan.GLC:
+                g.add_edges_from([
+                    (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
+                    (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
+                ])
+                nx.set_node_attributes(g, {1: {"ring": False},
+                                           2: {"chiral": Chirality.DOWN}, 3: {"chiral": Chirality.UP},
+                                           4: {"chiral": Chirality.DOWN}, 5: {"chiral": Chirality.UP}})
+            elif self == Glycan.FRU:
+                g.add_edges_from([
+                    (10, 2), (2, 3), (3, 4), (4, 5), (5, 10),
+                    (1, 2), (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
+                ])
+                nx.set_node_attributes(g, {3: {"chiral": Chirality.UP}, 4: {"chiral": Chirality.DOWN},
+                                           5: {"chiral": Chirality.UP}})
+            elif self == Glycan.MAN:
+                g.add_edges_from([
+                    (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
+                    (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15)
+                ])
+                nx.set_node_attributes(g, {2: {"chiral": Chirality.UP}, 3: {"chiral": Chirality.UP},
+                                           4: {"chiral": Chirality.DOWN}, 5: {"chiral": Chirality.UP}})
+            elif self == Glycan.GAL:
+                g.add_edges_from([
+                    (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
+                    (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
+                ])
+                nx.set_node_attributes(g, {2: {"chiral": Chirality.DOWN}, 3: {"chiral": Chirality.UP},
+                                           4: {"chiral": Chirality.UP}, 5: {"chiral": Chirality.UP}})
+            elif self == Glycan.TAL:
+                g.add_edges_from([
+                    (10, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 10),
+                    (1, 11), (2, 12), (3, 13), (4, 14), (5, 6), (6, 15),
+                ])
+                nx.set_node_attributes(g, {2: {"chiral": Chirality.UP}, 3: {"chiral": Chirality.UP},
+                                           4: {"chiral": Chirality.UP}, 5: {"chiral": Chirality.UP}})
+            self.value["struct"] = g
+
+        return self.value["struct"]
 
 
 def from_string(mono):
--- glyles/smiles/smiles.py
+++ glyles/smiles/smiles.py
@@ -1,6 +1,7 @@
 import networkx as nx
 
-from glyles.glycans.glycans import Glycan, Chirality
+from glyles.glycans.glycans import Glycan, Chirality, Atom
+from glyles.grammar.parse import parse
 
 
 class DFS:
@@ -35,7 +36,7 @@ class SMILES:
 
     def __construct(self, g, tree, node, first=False):
         """
-
+        TODO: Test from non-circular starting points
         Args:
             g (nx.Graph): graph of the molecule
             tree (nx.DiGraph): dfs-tree on that molecule
@@ -61,4 +62,58 @@ class SMILES:
             return output
 
 
-print(SMILES().write(Glycan.GLC.structure(), 1))
+class Merger:
+    def merge(self, t):
+        self.__mark(t, 0)
+        return self.__merge(t, 0, 1)
+
+    def __mark(self, t, node):
+        children = [x[1] for x in t.edges(node)]
+
+        if len(children) == 0:  # leaf
+            return
+        if len(children) > 2:
+            raise NotImplementedError("Glycans with maximal branching factor not implemented.")
+
+        for child, atom in zip(children, [Atom.Y, Atom.Z]):
+            binding = list(t.get_edge_data(node, child)["type"])
+            # t.nodes[child].structure().nodes[binding[2]]  # davon die OH gruppe als start für SMILES!
+            # t.nodes[node].structure().nodes[binding[4]]  # davon die OH gruppe duch X ersetzen
+            monomer = t.nodes[node]["type"].structure()
+            for _, x in monomer.edges(int(binding[4])):   # davon die OH gruppe duch X ersetzen
+                if monomer.nodes[x]["type"] == Atom.O and 11 <= x <= 15:
+                    monomer.nodes[x]["type"] = atom
+                    break
+
+    def __merge(self, t, node, start):
+        children = [x[1] for x in t.edges(node)]
+        me = SMILES().write(t.nodes[node]["type"].structure(), start)
+
+        if len(children) == 0:  # leaf
+            return me
+        if len(children) > 2:
+            raise NotImplementedError("Glycans with maximal branching factor not implemented.")
+
+        for child, atom in zip(children, ["Y", "Z"]):
+            binding = list(t.get_edge_data(node, child)["type"])
+            monomer = t.nodes[node]["type"].structure()
+            child_start = -1
+
+            for _, x in monomer.edges(int(binding[2])):  # davon die OH gruppe duch X ersetzen
+                if monomer.nodes[x]["type"] == Atom.O and 11 <= x <= 15:
+                    child_start = x
+                    break
+
+            if child_start == -1:
+                raise ValueError("SMILES cannot computed from atom -1!")
+
+            child_smiles = self.__merge(t, child, child_start)
+            me = me.replace(atom, child_smiles)
+
+        return me
+
+
+# print(SMILES().write(Glycan.GLC.structure(), 1))
+g = parse("Man(a1-4)[Fru(a1-3)]Gal")
+s = Merger().merge(g)
+print(s)
