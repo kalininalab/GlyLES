fb527d8fbc32af1c5bd1904d72e081086750f55a
Roman <joeres.roman@web.de>
2021-11-10 00:05:48 +0100

    Fixing bugs in parser test cases

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,10 +5,10 @@ smiles = [
     "Man(a1-4)Glc",
     "Man(a1-4)Glc(a1-3)Tal",
     "Man(a1-4)[Glc(a1-3)]Tal",
-    "Man(a1-4)[Glc(a1-3)Tal(b1-4)]Gal",
-    "Man(a1-4)Glc(a1-3)[Tal(b1-4)]Gal",
-    "Man(a1-4)Glc(a1-3)[Tal(b1-4)Gal(b1-3)]Tal",
-    "Man(a1-4)[Glc(a1-3)Tal(b1-4)]Gal(b1-3)Tal",
+    "Man(a1-2)[Glc(a1-3)Tal(b1-4)]Gal",
+    "Man(a1-2)Glc(a1-3)[Tal(b1-4)]Gal",
+    "Man(a1-4)Glc(a1-2)[Tal(b1-4)Gal(b1-3)]Tal",
+    "Man(a1-2)[Glc(a1-3)Tal(b1-4)]Gal(b1-3)Tal",  # !
 ]
 
 
@@ -33,19 +33,19 @@ def split_children(g, id_children, child_1):
 
 class TestParser:
     def test_parse_1(self):
-        g = Glycan(smiles[0], parse=False).get_tree()
+        g = Glycan(smiles[0]).get_tree()
 
         check_initial(g, "Man", 0)
 
     def test_parse_2(self):
-        g = Glycan(smiles[1], parse=False).get_tree()
+        g = Glycan(smiles[1]).get_tree()
 
         check_initial(g, "Glc", 1)
         id_child_1 = list(g.edges(0))[0][1]
         check_child(g, 0, id_child_1, "Man", "(a1-4)", 0)
 
     def test_parse_3(self):
-        g = Glycan(smiles[2], parse=False).get_tree()
+        g = Glycan(smiles[2]).get_tree()
 
         check_initial(g, "Tal", 1)
         id_child_1 = list(g.edges(0))[0][1]
@@ -54,7 +54,7 @@ class TestParser:
         check_child(g, id_child_1, id_child_2, "Man", "(a1-4)", 0)
 
     def test_parse_4(self):
-        g = Glycan(smiles[3], parse=False).get_tree()
+        g = Glycan(smiles[3]).get_tree()
 
         check_initial(g, "Tal", 2)
         id_children_1 = [x[1] for x in list(g.edges(0))]
@@ -64,20 +64,20 @@ class TestParser:
         check_child(g, 0, id_child_2, "Man", "(a1-4)", 0)
 
     def test_parse_5(self):
-        g = Glycan(smiles[4], parse=False).get_tree()
+        g = Glycan(smiles[4]).get_tree()
 
         check_initial(g, "Gal", 2)
         id_children_1 = [x[1] for x in list(g.edges(0))]
         id_child_1, id_child_2 = split_children(g, id_children_1, "Tal")
 
         check_child(g, 0, id_child_1, "Tal", "(b1-4)", 1)
-        check_child(g, 0, id_child_2, "Man", "(a1-4)", 0)
+        check_child(g, 0, id_child_2, "Man", "(a1-2)", 0)
 
         id_child_11 = list(g.edges(id_child_1))[0][1]
         check_child(g, id_child_1, id_child_11, "Glc", "(a1-3)", 0)
 
     def test_parse_6(self):
-        g = Glycan(smiles[5], parse=False).get_tree()
+        g = Glycan(smiles[5]).get_tree()
 
         check_initial(g, "Gal", 2)
         id_children_1 = [x[1] for x in list(g.edges(0))]
@@ -90,14 +90,14 @@ class TestParser:
         check_child(g, id_child_2, id_child_21, "Man", "(a1-4)", 0)
 
     def test_parse_7(self):
-        g = Glycan(smiles[6], parse=False).get_tree()
+        g = Glycan(smiles[6]).get_tree()
 
         check_initial(g, "Tal", 2)
         id_children_1 = [x[1] for x in list(g.edges(0))]
         id_child_1, id_child_2 = split_children(g, id_children_1, "Gal")
 
         check_child(g, 0, id_child_1, "Gal", "(b1-3)", 1)
-        check_child(g, 0, id_child_2, "Glc", "(a1-3)", 1)
+        check_child(g, 0, id_child_2, "Glc", "(a1-2)", 1)
 
         id_child_11 = list(g.edges(id_child_1))[0][1]
         check_child(g, id_child_1, id_child_11, "Tal", "(b1-4)", 0)
@@ -106,7 +106,7 @@ class TestParser:
         check_child(g, id_child_2, id_child_21, "Man", "(a1-4)", 0)
 
     def test_parse_8(self):
-        g = Glycan(smiles[7], parse=False).get_tree()
+        g = Glycan(smiles[7]).get_tree()
 
         check_initial(g, "Tal", 1)
         id_child_1 = list(g.edges(0))[0][1]
@@ -116,7 +116,7 @@ class TestParser:
         id_child_11, id_child_12 = split_children(g, id_children_1, "Tal")
 
         check_child(g, id_child_1, id_child_11, "Tal", "(b1-4)", 1)
-        check_child(g, id_child_1, id_child_12, "Man", "(a1-4)", 0)
+        check_child(g, id_child_1, id_child_12, "Man", "(a1-2)", 0)
 
         id_child_111 = list(g.edges(id_child_11))[0][1]
         check_child(g, id_child_11, id_child_111, "Glc", "(a1-3)", 0)
