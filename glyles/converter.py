e0f385fe8e0dd7b148661e786020d87558243c96
Roman <joeres.roman@web.de>
2021-11-16 13:14:22 +0100

    Global structure of access and private methods of the tool

---
+++ glyles/converter.py
@@ -0,0 +1,77 @@
+import os
+import sys
+
+from glyles.grammar.parse import Glycan
+
+
+def convert(glycan=None, glycan_list=None, glycan_file=None, output_file=None, generator=False, silent=True):
+    """
+    General user interaction interface to use this library.
+
+    idea: Implement a generator argument that arbitrarily generates a stream of glycans
+
+    Args:
+        glycan (str): Single glycan to be converted from IUPAC to SMILES
+        glycan_list (List[str]): list of glycans to convert
+        glycan_file (str): File to read the glycans from
+        output_file (str): File to save the conversions in
+        generator (bool): Flag indicating the use of this method as generator
+
+    Returns:
+        Nothing
+    """
+    glycans = []
+
+    # fill the list of glycans to convert
+    if glycan is not None:
+        glycans.append(glycan)
+    if glycan_list is not None:
+        glycans += glycan_list
+    if glycan_file is not None:
+        if not os.path.isfile(glycan_file):
+            pass
+        for line in open(glycan_file, "r").readlines():
+            glycans.append(line.strip())
+    if len(glycans) == 0:
+        if not silent:
+            print("List of glycans is empty")
+        return
+
+    # determine the output format
+    # TODO: Check for generator, a generator makes this unnecessary
+    if output_file is not None:
+        if os.path.isdir(os.path.dirname(output_file)):
+            output = open(output_file, "w")
+        else:
+            if not silent:
+                print("Path of output-file does not exist! Results will be printed on stdout.", file=sys.stderr)
+            output = sys.stdout
+    else:
+        if not silent:
+            print("No output-file specified, results will be printed on stdout.")
+        output = sys.stdout
+
+    # Convert the glycans ...
+    for glycan in glycans:
+        try:
+            # ... by passing them to the glycan class to parse them ...
+            smiles = Glycan(glycan).get_smiles()
+
+            # ... and return them as intended
+            if generator:
+                yield smiles
+            else:
+                print(glycan, smiles, file=output, sep=",")
+        # catch any exception at glycan level to not destroy the whole pipeline because of one mis-formed glycan
+        except Exception as e:
+            print(f"An exception occurred with {glycan}:", e.__class__, file=sys.stderr)
+            print("Error message:", e.__str__(), file=sys.stderr)
+            if generator:
+                yield ""
+
+    output.close()  # not sure if this works for stdout
+
+
+if __name__ == '__main__':  # testing
+    for smiles in convert(glycan="Man(a1-2)Man", output_file="./blbabl/out.txt"):
+        print(smiles)
--- glyles/grammar/parse.py
+++ glyles/grammar/parse.py
@@ -9,7 +9,6 @@ from glyles.glycans.nx_monomer import NXMonomer
 from glyles.glycans.rdkit_monomer import RDKitMonomer
 from glyles.grammar.GlycanLexer import GlycanLexer
 from glyles.grammar.GlycanParser import GlycanParser
-from glyles.utils.merge import Merger
 
 
 class Glycan:
@@ -22,7 +21,7 @@ class Glycan:
     resulting abstract syntax trees (AST)s are not intuitive.
     """
 
-    class TreeWalker:
+    class __TreeWalker:
         def __init__(self):
             """
             Just initialize an empty tree
@@ -147,6 +146,96 @@ class Glycan:
 
             return node_id
 
+    class __Merger:
+        """
+        Merge the tree of monomers into a SMILES representation of the complete molecule.
+        """
+
+        def merge(self, t, root_orientation="n", start=10):
+            """
+            Merge the provided tree of monomers enriched with the glycans in the nodes and information on the bindings
+            between two monomer-nodes in the edges.
+
+            Args:
+                t (networkx.DiGraph): Graph representing the glycan to compute the whole SMILES representation for.
+                root_orientation (str): Orientation of the root monomer in glycan (can be a (= alpha) or b (= beta))
+                start (int): index of the atom to start the smiles generation from
+
+            Returns:
+                SMILES representation as string
+            """
+            # first mark the atoms that will be replaced in a binding of two monomers
+            self.__mark(t, 0, "({}1-?)".format(root_orientation))
+
+            # return the string that can be computed from connecting the monomers as marked above
+            return self.__merge(t, 0, start, 1)
+
+        def __mark(self, t, node, p_edge):
+            """
+            Recursively mark in every node of the molecule which atoms are being replaced by bound monomers.
+
+            Args:
+                t (networkx.DiGraph): Tree representing the glycan to compute the whole SMILES representation for.
+                node (int): ID of the node to work on in this method
+                p_edge (str): edge annotation to parent monomer
+
+            Returns:
+                Nothing
+            """
+            # get children nodes
+            children = [x[1] for x in t.edges(node)]
+
+            # set chirality of atom binding parent
+            if p_edge is not None and t.nodes[node]["type"].is_non_chiral():
+                t.nodes[node]["type"] = t.nodes[node]["type"].to_chirality(p_edge[1])
+
+            # check for validity of the tree, i.e. if its a leaf (return, nothing to do) or has too many children (Error)
+            if len(children) == 0:  # leaf
+                return
+            if len(children) > 3:  # too many children
+                raise NotImplementedError("Glycans with maximal branching 4 factor not implemented.")
+
+            # iterate over the children and the atoms used to mark binding atoms in my structure
+            for child, atom in zip(children, t.nodes[node]["type"].get_dummy_atoms()[0]):
+                binding = t.get_edge_data(node, child)["type"]
+
+                t.nodes[node]["type"].mark(int(binding[4]), atom)
+                self.__mark(t, child, binding)
+
+        def __merge(self, t, node, start, ring_index):
+            """
+            Recursively merge every node of the molecule with its children and get the SMILES representation of the subtree.
+
+            Args:
+                t (networkx.DiGraph): Graph representing the glycan to compute the whole SMILES representation for.
+                node (int): ID of the node to work on in this method
+                start (int): ID of the atom in the inner graph to start from when generating the SMILES string
+
+            Returns:
+                SMILES representation of the subtree of the given node
+            """
+            # get my children and compute my SMILES string
+            children = [x[1] for x in t.edges(node)]
+            me = t.nodes[node]["type"].to_smiles(start, ring_index)
+            # check for validity of the tree, i.e. if its a leaf (return, nothing to do) or has too many children (Error)
+            if len(children) == 0:  # leaf
+                return me
+            if len(children) > 3:  # too many children
+                raise NotImplementedError("Glycans with maximal branching factor 4 not implemented.")
+
+            # iterate over the children and the atoms used to mark binding atoms
+            for child, atom in zip(children, t.nodes[node]["type"].get_dummy_atoms()[1]):
+                binding = list(t.get_edge_data(node, child)["type"])
+
+                child_start = t.nodes[node]["type"].root_atom_id(int(binding[2]))
+                if child_start == -1:
+                    raise ValueError("No child start found.")
+
+                # get the SMILES of this child and plug it in in the current own SMILES
+                child_smiles = self.__merge(t, child, child_start, ring_index + 1)
+                me = me.replace(atom, child_smiles)
+            return me
+
     class Mode(Enum):
         """
         Enumerate different modes how to represent the monomers in the tree.
@@ -170,7 +259,7 @@ class Glycan:
         self.root_orientation = root_orientation
         self.start = start
         self.parse_smiles = parse
-        self.parse()
+        self.__parse()
 
     def get_smiles(self):
         """
@@ -180,7 +269,7 @@ class Glycan:
             Generated SMILES string
         """
         if self.glycan_smiles is None:
-            self.glycan_smiles = Merger().merge(self.parse_tree, self.root_orientation, start=self.start)
+            self.glycan_smiles = Glycan.__Merger().merge(self.parse_tree, self.root_orientation, start=self.start)
 
         return self.glycan_smiles
 
@@ -218,7 +307,7 @@ class Glycan:
         """
         nx.drawing.nx_pydot.write_dot(self.parse_tree, os.path.join(output, f"{self.iupac}.dot"))
 
-    def parse(self):
+    def __parse(self):
         """
         Adapter on the Lexer and Parser generated by ANTLR based on Grammar.g4.
 
@@ -233,10 +322,10 @@ class Glycan:
         tree = parser.start()
 
         # walk through the AST and parse the AST into a networkx representation of the glycan.
-        self.parse_tree = Glycan.TreeWalker().parse(tree, self.mode)
+        self.parse_tree = Glycan.__TreeWalker().parse(tree, self.mode)
 
         if self.parse_smiles:
-            self.glycan_smiles = Merger().merge(self.parse_tree, self.root_orientation, start=self.start)
+            self.glycan_smiles = Glycan.__Merger().merge(self.parse_tree, self.root_orientation, start=self.start)
 
     @staticmethod
     def monomer_from_string(mono, mode):
