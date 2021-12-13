edd13bec5eec28c315773812216ca40c5db9cddd
Roman <joeres.roman@web.de>
2021-12-13 13:25:12 +0100

    New tests and renaming of old test. Also bug-fixing according to new tests

--- glyles/converter.py
+++ glyles/converter.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 
+from glyles.glycans.factory import MonomerFactory
 from glyles.grammar.parse import Glycan
 
 
@@ -46,9 +47,8 @@ def parsable_glycan(glycan):
         True if the glycan can be parsed in the parser
     """
     glycan = re.sub("[\(].*?[\)]", "", glycan)
-    glycan = glycan.replace("[", "").replace("]", "")
-    for monomer in ('Fuc', 'GalNAc4S', 'GalNAc6S', 'GalNAc', 'Gal3S', 'Gal6S', 'Gal',
-                    'GlcNAc6S', 'GlcNAc', 'GlcA', 'Glc', 'Man', 'Tal'):
+    glycan = glycan.replace("[", "").replace("]", "").upper()
+    for monomer in MonomerFactory.monomers():
         glycan = glycan.replace(monomer, " ")
     return len(glycan.replace(" ", "")) == 0
 
@@ -64,12 +64,14 @@ def convert(glycan=None, glycan_list=None, glycan_file=None, glycan_generator=No
         glycan_file (str): File to read the glycans from
         glycan_generator (generator): generator yielding iupac representation.
             Together with output_generator=True this does not create any lists
-        output_file (str): File to save the conversions in
-        silent (bool): Flag indicating to have no output from this method
+        output_file (str): File to save the converted glycans in
+        returning (bool): Flag indicating to return a list of tuples
+        silent (bool): Flag indicating to have no prints from this method
 
     Returns:
         Nothing
     """
+
     glycans = preprocess_glycans(glycan, glycan_list, glycan_file)
     if len(glycans) == 0 and glycan_generator is None:
         if not silent:
@@ -78,7 +80,7 @@ def convert(glycan=None, glycan_list=None, glycan_file=None, glycan_generator=No
 
     # determine the output format
     if output_file is not None:
-        if os.path.isdir(os.path.dirname(output_file)):
+        if os.path.isdir(os.path.dirname(os.path.abspath(output_file))):
             output = open(output_file, "w")
         else:
             if not silent:
@@ -92,30 +94,24 @@ def convert(glycan=None, glycan_list=None, glycan_file=None, glycan_generator=No
                 print("No output-file specified, results will be printed on stdout.")
             output = sys.stdout
 
-    if glycan_generator is not None:
-        glycans = glycans + glycan_generator
-
-    # Convert the glycans ...
-    for glycan in glycans:
-        try:
-            # ... by passing them to the glycan class to parse them ...
-            if parsable_glycan(glycan):
-                smiles = Glycan(glycan).get_smiles()
-            else:
-                print(f"Glycan {glycan} is not parsable due to unknown monomers.", file=sys.stderr)
-                continue
-
+    print(len(glycans))
+    if len(glycans) != 0:
+        for glycan, smiles in convert_generator(glycan_list=glycans):
             if returning:
-                output.append(smiles)
+                output.append((glycan, smiles))
             else:
                 # ... and return them as intended
                 print(glycan, smiles, file=output, sep=",")
 
-        # catch any exception at glycan level to not destroy the whole pipeline because of one mis-formed glycan
-        except Exception as e:
-            print(f"An exception occurred with {glycan}:", e.__class__, file=sys.stderr)
-            print("Error message:", e.__str__(), file=sys.stderr)
+    if glycan_generator is not None:
+        for glycan, smiles in convert_generator(glycan_generator=glycan_generator):
+            if returning:
+                output.append((glycan, smiles))
+            else:
+                # ... and return them as intended
+                print(glycan, smiles, file=output, sep=",")
 
+    print(len(output))
     if returning:
         return output
     else:
@@ -137,35 +133,43 @@ def convert_generator(glycan=None, glycan_list=None, glycan_file=None, glycan_ge
     Returns:
         Nothing
     """
+
     glycans = preprocess_glycans(glycan, glycan_list, glycan_file)
-    if len(glycans) == 0:
+    if len(glycans) == 0 and glycan_generator is None:
         if not silent:
             print("List of glycans is empty")
         return
 
-    if glycan_generator is not None:
-        glycans = glycans + glycan_generator
+    # Convert the glycans ...
+    if len(glycans) != 0:
+        for glycan in glycans:
+            try:
+                # ... by passing them to the glycan class to parse them and return them as intended
+                if parsable_glycan(glycan):
+                    yield glycan, Glycan(glycan).get_smiles()
+                else:
+                    print(f"Glycan {glycan} is not parsable due to unknown monomers.", file=sys.stderr)
+                    yield glycan, ""
+
+            # catch any exception at glycan level to not destroy the whole pipeline because of one mis-formed glycan
+            except Exception as e:
+                print(f"An exception occurred with {glycan}:", e.__class__, file=sys.stderr)
+                print("Error message:", e.__str__(), file=sys.stderr)
+                yield glycan, ""
 
     # Convert the glycans ...
-    for glycan in glycans:
-        try:
-            # ... by passing them to the glycan class to parse them ...
-            if parsable_glycan(glycan):
-                smiles = Glycan(glycan).get_smiles()
-            else:
-                print(f"Glycan {glycan} is not parsable due to unknown monomers.", file=sys.stderr)
-                continue
+    if glycan_generator is not None:
+        for glycan in glycan_generator:
+            try:
+                # ... by passing them to the glycan class to parse them and return them as intended
+                if parsable_glycan(glycan):
+                    yield glycan, Glycan(glycan).get_smiles()
+                else:
+                    print(f"Glycan {glycan} is not parsable due to unknown monomers.", file=sys.stderr)
+                    yield glycan, ""
 
-            # ... and return them as intended
-            yield smiles
-
-        # catch any exception at glycan level to not destroy the whole pipeline because of one mis-formed glycan
-        except Exception as e:
-            print(f"An exception occurred with {glycan}:", e.__class__, file=sys.stderr)
-            print("Error message:", e.__str__(), file=sys.stderr)
-            yield ""
-
-
-if __name__ == '__main__':  # testing
-    for smiles in convert(glycan="Man(a1-2)Man", output_file="./blbabl/out.txt"):
-        print(smiles)
+            # catch any exception at glycan level to not destroy the whole pipeline because of one mis-formed glycan
+            except Exception as e:
+                print(f"An exception occurred with {glycan}:", e.__class__, file=sys.stderr)
+                print("Error message:", e.__str__(), file=sys.stderr)
+                yield glycan, ""
---
+++ tests/test_convert.py
@@ -0,0 +1,96 @@
+import os.path
+
+from glyles.converter import convert, convert_generator
+from tests.test_smiles import compare_smiles
+
+
+def setup_test():
+    def generator(x_list):
+        for x in x_list:
+            yield x
+
+    def write_file(x_list, name="./test.txt"):
+        f = open(name, "w")
+        for x in x_list:
+            f.write(x + "\n")
+        f.close()
+        return name
+
+    return {
+        "glycan": TestConverter.smiles_samples[0][0],
+        "glycan_list": [TestConverter.smiles_samples[1][0], TestConverter.smiles_samples[2][0]],
+        "glycan_file": write_file([TestConverter.smiles_samples[i][0] for i in range(3, 8)]),
+        "glycan_generator": generator([TestConverter.smiles_samples[i][0] for i in range(8, 13)])
+    }
+
+
+def check_results(output):
+    solution = [TestConverter.smiles_samples[i][1] for i in range(13)]
+    i = 0
+    for i, ((o_glycan, o_smiles), s_smiles) in enumerate(zip(output, solution)):
+        assert o_glycan == TestConverter.smiles_samples[i][0]
+        compare_smiles(o_smiles, s_smiles)
+    assert i == 12
+    clear_test()
+
+
+def clear_test():
+    if os.path.exists("./test.txt"):
+        os.remove("./test.txt")
+    if os.path.exists("./output.txt"):
+        os.remove("./output.txt")
+
+
+class TestConverter:
+    smiles_samples = [
+        ("Gal(a1-4)Gal",
+         "OC[C@H]1O[C@H](O[C@H]2[C@@H](CO)OC(O)[C@H](O)[C@H]2O)[C@H](O)[C@@H](O)[C@H]1O"),
+        ("Man(a1-3)[Man(a1-6)]Man",
+         "OC[C@H]3O[C@H](OC[C@H]2OC(O)[C@@H](O)[C@@H](O[C@H]1O[C@H](CO)[C@@H](O)[C@H](O)[C@@H]1O)[C@@H]2O)[C@@H](O)"
+         "[C@@H](O)[C@@H]3O"),
+        ("Man(a1-2)Man(a1-3)[Man(a1-3)Man(a1-6)]Man",
+         "OC[C@H]5O[C@H](O[C@H]4[C@H](O)[C@@H](CO)O[C@H](OC[C@H]3OC(O)[C@@H](O)[C@@H](O[C@H]1O[C@H](CO)[C@@H](O)"
+         "[C@H](O)[C@@H]1O[C@H]2O[C@H](CO)[C@@H](O)[C@H](O)[C@@H]2O)[C@@H]3O)[C@H]4O)[C@@H](O)[C@@H](O)[C@@H]5O"),
+        ("Gal(b1-4)Glc",
+         "OC[C@H]2O[C@@H](O[C@H]1[C@H](O)[C@@H](O)C(O)O[C@@H]1CO)[C@H](O)[C@@H](O)[C@H]2O"),
+        ("Gal(b1-3)Glc",
+         "OC[C@H]2O[C@@H](O[C@@H]1[C@@H](O)C(O)O[C@H](CO)[C@H]1O)[C@H](O)[C@@H](O)[C@H]2O"),
+        ("Glc(b1-3)Glc",
+         "OC[C@H]2O[C@@H](O[C@@H]1[C@@H](O)C(O)O[C@H](CO)[C@H]1O)[C@H](O)[C@@H](O)[C@@H]2O"),
+        ("Gal(b1-4)Gal",
+         "OC[C@H]2O[C@@H](O[C@@H]1[C@H](O)[C@@H](O)C(O)O[C@@H]1CO)[C@H](O)[C@@H](O)[C@H]2O"),
+        ("Man(a1-4)Man",
+         "OC[C@H]2O[C@H](O[C@H]1[C@H](O)[C@H](O)C(O)O[C@@H]1CO)[C@@H](O)[C@@H](O)[C@@H]2O"),
+        ("Man(a1-3)Man",
+         "OC[C@H]2O[C@H](O[C@@H]1[C@H](O)C(O)O[C@H](CO)[C@H]1O)[C@@H](O)[C@@H](O)[C@@H]2O"),
+        ("Gal(a1-4)Gal(b1-4)Glc",
+         "OC[C@H]3O[C@H](O[C@@H]2[C@H](O)[C@@H](O)[C@H](O[C@H]1[C@H](O)[C@@H](O)C(O)O[C@@H]1CO)O[C@@H]2CO)[C@H](O)"
+         "[C@@H](O)[C@H]3O"),
+        ("Gal(a1-3)Gal",
+         "OC[C@H]2O[C@H](O[C@@H]1[C@@H](O)C(O)O[C@H](CO)[C@@H]1O)[C@H](O)[C@@H](O)[C@H]2O"),
+        ("Glc(a1-3)Glc",
+         "OC[C@H]2O[C@H](O[C@@H]1[C@@H](O)C(O)O[C@H](CO)[C@H]1O)[C@H](O)[C@@H](O)[C@@H]2O"),
+        ("Man(a1-2)Man",
+         "OC[C@H]2OC(O)[C@@H](O[C@H]1O[C@H](CO)[C@@H](O)[C@H](O)[C@@H]1O)[C@@H](O)[C@@H]2O"),
+    ]
+
+    def test_file_output(self):
+        args = setup_test()
+
+        convert(args["glycan"], args["glycan_list"], args["glycan_file"], args["glycan_generator"],
+                output_file="./output.txt")
+        output = [line.split(",") for line in open("./output.txt", "r").readlines()]
+        check_results(output)
+
+    def test_return_output(self):
+        args = setup_test()
+
+        output = convert(args["glycan"], args["glycan_list"], args["glycan_file"], args["glycan_generator"],
+                         returning=True)
+        check_results(output)
+
+    def test_generator(self):
+        args = setup_test()
+
+        output = convert_generator(args["glycan"], args["glycan_list"], args["glycan_file"], args["glycan_generator"])
+        check_results(output)
