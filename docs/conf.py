69830c9e9429957875ad54759a2ce74d04a10cef
Roman <joeres.roman@web.de>
2022-10-21 20:23:17 +0200

    glycan docu 6

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import sys
 
 import sphinx_rtd_theme
 
-sys.path.append(os.path.abspath("../.."))
+sys.path.append(os.path.abspath(".."))
 
 import glyles
 
@@ -31,7 +31,7 @@ author = "Roman Joeres"
 project = "GlyLES"
 copyright = f"{datetime.datetime.now().year}, {author}"
 
-version = list(open("../../pyproject.toml", "r").readlines())[2].strip().split("\"")[1].split("\"")[0]
+version = list(open("../pyproject.toml", "r").readlines())[2].strip().split("\"")[1].split("\"")[0]
 release = version
 
 html_theme = "sphinx_rtd_theme"
