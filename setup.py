ad7f4bae5e21a8e1969de33b0a448a49377275e3
alperyurtseven <yurtsevenalper@gmail.com>
2023-01-19 14:16:59 +0100

    corrected conda files

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.5.1",
     description="Convert the IUPAC into a SMILES representation",
     author="Roman Joeres",
-    scripts=["glyles/command_line_caller.py"]
+    scripts=["glyles/glyles.py"]
 )
