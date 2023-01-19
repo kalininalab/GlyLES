4b29a3f5e1d510e12c1e2f18fb05db4e14133a16
alperyurtseven <yurtsevenalper@gmail.com>
2023-01-19 11:58:48 +0100

    corrected conda files

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name="glyles",
     install_requires=[
-        "colorama; platform_system == 'Windows'",
         "importlib-metadata; python_version < '3.8'",
     ],
 )
