5eb82e4fdd90a69798306ba9e79273bf6ede7a4f
Old-Shatterhand <joeres.roman@web.de>
2025-08-19 19:33:15 +0200

    Minor fix

--- glyles/__init__.py
+++ glyles/__init__.py
@@ -2,4 +2,3 @@ from .converter import convert, convert_generator
 
 from .glycans.poly.glycan import Glycan
 from .glycans.poly.gwb_glycan import GWBGlycan
-from .glycans.poly.wurcs_glycan import WURCSGlycan
